bdb44452d55efd295d3154eea59a701771c4e797
pogo <pogster@gmail.com>
2013-11-25 10:58:50 -0500

    initial commit

---
+++ MtgoxClass.py
@@ -0,0 +1,80 @@
+#IMPORTANT NOTES:
+#All API methods are cached for 10 seconds. Do not request results more often than that, you might be blocked by the anti-DDoS filters. 
+class Mtgox:
+
+	def __init__ (self):
+
+		#load our API public Key
+		fobj = open("publickey.txt")
+		self.key = str(fobj.read()) #warning - readlines() returns a List obj
+		fobj.close()
+
+		#load our API private Key
+		fobj = open("privatekey.txt")
+		self.sec = str(fobj.read())
+		fobj.close()
+
+	def __sign(self, path, data):
+		import hmac, base64, hashlib
+		
+		mac = hmac.new(base64.b64decode(self.sec), path+chr(0)+data, hashlib.sha512)
+		return base64.b64encode(str(mac.digest()))
+
+	def auth(self, path, inp={}, get=False):
+		import time, requests, json, urllib, urllib2
+
+		base = 'https://data.mtgox.com/api/2/'
+		
+		if get:
+			#GET
+			get_data = urllib.urlencode(inp) 
+			r = requests.get(base+path,data=get_data)
+			data_string = json.dumps(r.json(), indent=2)
+		
+		else:
+			inp['tonce'] = str(int(time.time()*1e6)) #fill inp dict with tonce value
+			data = urllib.urlencode(inp) #URL Encode the tonce value
+			restsign = self.__sign(path, data)
+
+			#POST
+			headers = {'Rest-Key':  self.key,
+			           'Rest-Sign': restsign,
+			           'Content-Type': 'application/x-www-form-urlencoded'}
+			r = requests.post(base+path, data=inp, headers=headers)
+			#print r.request.headers
+
+		return r
+
+	
+	
+
+#!! REMEMBER !!
+#Bid refers to buying BTC using the auxiliary currency (e.g. USD)
+#Ask refers to selling BTC for the auxiliary currency
+
+
+
+
+
+
+#Will throw a python error if POST fails
+#r.raise_for_status()
+
+#GET example 
+#path = 'BTCUSD/money/ticker_fast'
+#r = auth(path, {}, True)
+#print json.dumps(json.loads(r.text), sort_keys = True, indent = 2)
+
+
+#Encoding to JSON
+#data = [ { 'a':'A', 'b':(2, 4), 'c':3.0 } ]
+#print 'ORIGINAL Data' , data
+#data_string = json.dumps(data)
+#print 'ENCODED:', data_string
+
+#Decoding from JSON
+#decoded = json.loads(data_string)
+#print 'DECODED:', decoded
+#print 'ORIGINAL:', type(data[0]['b'])
+#print 'DECODED :', type(decoded[0]['b'])
+
---
+++ traderV2.py
@@ -0,0 +1,114 @@
+from MtgoxClass import Mtgox
+import json, time, decimal as D
+from pprint import pprint
+
+gox = Mtgox() #create instance of mtgox obj
+
+
+
+#Ticker
+r = gox.auth('BTCUSD/money/ticker',{})
+print json.dumps(r.json(), sort_keys = True, indent=4, separators=(',', ': '))
+j = r.json()
+#print j['data'].keys() #get members of dict branch
+print time.strftime("%m/%d/%Y %H:%M:%S", time.localtime(D.Decimal(j['data']['now'])/1000000)) 
+print 'avg: ' + j['data']['avg']['value']
+print 'buy: ' + j['data']['buy']['value']
+print 'sell: ' + j['data']['sell']['value']
+print 'high: ' + j['data']['high']['value']
+print 'low: ' + j['data']['low']['value']
+print 'vol: ' + j['data']['vol']['value']
+print 'vwap: ' + j['data']['vwap']['value']
+print ''
+
+'''
+
+#Account / Wallet Info
+time.sleep(10)
+r = gox.auth('BTCUSD/money/info',{})
+#print json.dumps(r.json(), sort_keys = True, indent=4, separators=(',', ': '))
+j = r.json()
+#print j['data'].keys()
+btc_bal = D.Decimal(j['data']['Wallets']['BTC']['Balance']['value_int'])/100000000
+usd_bal = D.Decimal(j['data']['Wallets']['USD']['Balance']['value_int'])/100000
+print 'BTC Balance: ', btc_bal
+print 'USD Balance: ', usd_bal
+print ''
+
+'''
+
+'''
+
+#Get a spot quote to BUY BTC with USD
+time.sleep(10)
+btc_to_buy = input('How many BTC to Buy?')
+r = gox.auth('BTCUSD/money/order/quote',{'type':'bid','amount':btc_to_buy*100000000})
+#print json.dumps(r.json(), sort_keys = True, indent=4, separators=(',', ': '))
+j = r.json()
+print 'USD Cost: $' + str(D.Decimal(j['data']['amount'])/100000)
+print '' 
+
+'''
+'''
+
+#Get a spot quote to SELL BTC for USD
+time.sleep(10)
+btc_to_sell = input('How many BTC to Sell?')
+r = gox.auth('BTCUSD/money/order/quote',{'type':'ask','amount':btc_to_sell*100000000})
+#print json.dumps(r.json(), sort_keys = True, indent=4, separators=(',', ': '))
+j = r.json()
+print 'USD Income: $' + str(D.Decimal(j['data']['amount'])/100000)
+print ''
+
+'''
+'''
+
+#Pull in Trade History
+#time.sleep(10)
+r = gox.auth('BTCUSD/money/wallet/history',{'currency':'USD'})
+print json.dumps(r.json(), sort_keys = True, indent=4, separators=(',', ': '))
+j = r.json()
+
+print 'Total Records: ', j['data']['records']
+
+records = j['data']['result']
+x = 0
+for record in records:
+	
+	pprint(j['data']['result'][x]['Info'])
+	print'TID:   ', j['data']['result'][x]['Trade']['tid']
+	print'UUID:  ', j['data']['result'][x]['Link'][0]
+	print'Type:  ', j['data']['result'][x]['Type']
+	print'Value: ', j['data']['result'][x]['Value']['value']
+	print'Currency Out: ', j['data']['result'][x]['Trade']['Amount']['currency']
+	print'Amount Out: ', j['data']['result'][x]['Trade']['Amount']['value']
+	print'Price :', round(D.Decimal(j['data']['result'][x]['Value']['value'])/D.Decimal(j['data']['result'][x]['Trade']['Amount']['value']),5)
+
+	x = x + 1
+	print ''
+
+'''
+
+'''
+time.sleep(10)
+r = gox.auth('BTCUSD/money/depth/fetch',{})	
+
+#print json.dumps(r.json(), sort_keys = True, indent=4, separators=(',', ': '))
+j = r.json()
+
+print ['stamp','price','amount']
+x = 0
+for item in j['data']['asks']:
+	print [time.strftime("%m/%d/%Y %H:%M:%S", time.localtime(D.Decimal(j['data']['asks'][x]['stamp'])/1000000)),j['data']['asks'][x]['price'],j['data']['asks'][x]['amount']]
+	x+=1
+x = 0
+for item in j['data']['bids']:
+	print [time.strftime("%m/%d/%Y %H:%M:%S", time.localtime(D.Decimal(j['data']['bids'][x]['stamp'])/1000000)),j['data']['bids'][x]['price'],j['data']['bids'][x]['amount']]
+	x+=1
+
+print 'Max Price: ' + j['data']['filter_max_price']['value'] #in USD
+print 'Min Price: ' + j['data']['filter_min_price']['value'] #in USD
+'''
+
+
+
