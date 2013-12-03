286f9d3b8ef07bea57052b03608668f34ae59e7d
fitofpassion <pogster@gmail.com>
2013-12-03 17:01:21 -0500

    debugging

--- MtgoxClass.py
+++ MtgoxClass.py
@@ -27,10 +27,12 @@ class Mtgox:
 		
 		if get:
 			#GET
+			inp['tonce'] = str(int(time.time()*1e6))
 			get_data = urllib.urlencode(inp) 
 			r = requests.get(base+path,data=get_data)
 			#data_string = json.dumps(r.json(), indent=2)
-			#print r.request.headers
+			print inp
+			print get_data
 		else:
 			inp['tonce'] = str(int(time.time()*1e6)) #fill inp dict with tonce value
 			data = urllib.urlencode(inp) #URL Encode the tonce value
